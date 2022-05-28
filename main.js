998d0347a96af9d477ccd2d6df5601f966c2b05d
lea <73567807+lebyleafy@users.noreply.github.com>
2022-05-28 16:14:24 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+
+const ages = new Charty({
+	title: 'Tổng chi phí: 7.850.000 VNĐ',
+	data: [
+		{'Chi phí mời diễn giả': 6900000},
+		{'Sách': 350000},
+		{'Tạo website': 300000},
+		{'Chi phí quảng cáo trên fanpage': 200000}
+	]
+});
